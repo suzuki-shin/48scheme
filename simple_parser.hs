df3af761c51aee1abed9e49edbef5b2d2b92fb9b
Shinichiro Suzuki <shinichiro.su@gmail.com>
2014-04-19 14:02:56 +0900

    スペースを無視させるようにした

--- simple_parser.hs
+++ simple_parser.hs
@@ -2,17 +2,19 @@
 import Text.ParserCombinators.Parsec hiding (spaces)
 import System.Environment (getArgs)
 
-symbol :: Parser Char
-symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
-
-readExpr :: String -> String
-readExpr input = case parse symbol "lisp" input of
-  Left err -> "No match: " ++ show err
-  Right val -> "Found value"
-
 main :: IO ()
 main = do
   args <- getArgs
 --   print $ args!!0
   putStrLn (readExpr (args !! 0))
 
+readExpr :: String -> String
+readExpr input = case parse (spaces >> symbol) "lisp" input of
+  Left err -> "No match: " ++ show err
+  Right val -> "Found value"
+
+symbol :: Parser Char
+symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
+
+spaces :: Parser ()
+spaces = skipMany1 space
