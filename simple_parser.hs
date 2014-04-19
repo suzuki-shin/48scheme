9329a7c5ca1069c0b9ea7b072b43b96c6f6318cc
Shinichiro Suzuki <shinichiro.su@gmail.com>
2014-04-19 14:26:23 +0900

    Lispのデータ型を定義した

--- simple_parser.hs
+++ simple_parser.hs
@@ -1,6 +1,14 @@
 {-# OPTIONS_GHC -Wall #-}
 import Text.ParserCombinators.Parsec hiding (spaces)
 import System.Environment (getArgs)
+import Control.Monad (liftM)
+
+data LispVal = Atom String
+             | List [LispVal]
+             | DottedList [LispVal] LispVal
+             | Number Integer
+             | String String
+             | Bool Bool
 
 main :: IO ()
 main = do
@@ -9,12 +17,37 @@ main = do
   putStrLn (readExpr (args !! 0))
 
 readExpr :: String -> String
-readExpr input = case parse (spaces >> symbol) "lisp" input of
-  Left err -> "No match: " ++ show err
-  Right val -> "Found value"
+readExpr input = case parse parseExpr "lisp" input of
+    Left err -> "No match: " ++ show err
+    Right _ -> "Found value"
 
 symbol :: Parser Char
 symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
 
 spaces :: Parser ()
 spaces = skipMany1 space
+
+parseString :: Parser LispVal
+parseString = do
+  char '"'
+  x <- many (noneOf "\"")
+  char '"'
+  return $ String x
+
+parseAtom :: Parser LispVal
+parseAtom = do first <- letter <|> symbol
+               rest <- many (letter <|> digit <|> symbol)
+               let atom = first:rest
+               return $ case atom of
+                          "#t" -> Bool True
+                          "#f" -> Bool False
+                          _    -> Atom atom
+
+parseNumber :: Parser LispVal
+parseNumber = liftM (Number . read) $ many1 digit
+
+parseExpr :: Parser LispVal
+parseExpr = parseAtom
+        <|> parseString
+        <|> parseNumber
+
